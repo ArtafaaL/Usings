b19b598875c315a051ccfa4450a089a9f12a4195
ArtafaaL <158111450+ArtafaaL@users.noreply.github.com>
2024-03-19 12:49:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 //Написать пример, приложения, читающего файл из потока(Stream)
 
